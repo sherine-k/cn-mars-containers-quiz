a219470e32dca851a95b07040059275c50048fd8
sherine-k <skhoury@redhat.com>
2025-03-05 17:30:18 +0100

    Fix jq for q7

--- q7.sh
+++ q7.sh
@@ -18,7 +18,7 @@ pe 'skopeo copy docker://registry.redhat.io/ubi9/ubi-micro:latest docker://cpe6m
 p 'Inspection de l'\''image: Son digest? celui de sa layer principale?'
 pe 'skopeo inspect docker://cpe6munf.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest'
 digest=`skopeo inspect docker://cpe6munf.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest | jq .Digest | tr -d '"'`
-blob_digest=`skopeo inspect docker://cpe6munf.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest | jq .Layers.[0]| tr -d '"'`
+blob_digest=`skopeo inspect docker://cpe6munf.c1.de1.container-registry.ovh.net/public/ubi9/ubi-micro:latest | jq .Layers[0]| tr -d '"'`
 p 'Suppression de l'\''image'
 p "curl -X DELETE https://cpe6munf.c1.de1.container-registry.ovh.net/v2/public/ubi9/ubi-micro/manifests/$digest"
 curl -u sherine:Sh\&r1n\&@ -X DELETE https://cpe6munf.c1.de1.container-registry.ovh.net/v2/public/ubi9/ubi-micro/manifests/$digest
@@ -30,3 +30,4 @@ curl https://cpe6munf.c1.de1.container-registry.ovh.net/v2/public/ubi9/ubi-micro
 pe 'ls -l /tmp/blob'
 
 p "Fini !"
+
